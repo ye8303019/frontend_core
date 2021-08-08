461156f91fde454457f8f15651f71ed4a9c0b1ce
zhongkai.ye <yezhongkai@patsnap.com>
2021-08-08 16:54:35 +0800

    PDLS-001 Init Commit

--- dist/0 - this/example_17.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var a = 123;
-console.log(undefined);
-//# sourceMappingURL=example_17.js.map
--- dist/dist/scope/example1.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var b = 'bar';
-function foo() {
-    console.log(b); //在 foo 的作用域中没有找变量 b，所以向外扩大作用域查找，在全局下找到 b
-}
-foo();
-//# sourceMappingURL=example1.js.map
